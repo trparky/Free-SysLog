b720e680816ba25e299f0cb68b120f1cbe1a17da
Thomas Parkison <trparky@toms-world.org>
2023-08-10 14:54:10 -0400

    Updated copyright in AssemblyInfo.vb.

--- Free SysLog/My Project/AssemblyInfo.vb
+++ Free SysLog/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("Free SysLog")> 
-<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyCopyright("Copyright Thomas Parkison © 2023-2024")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
