fba4bd61cd6892c0114b49d86cb856f1dc1e23d6
Thomas Parkison <trparky@toms-world.org>
2024-01-25 14:03:28 -0500

    Simplified another interpolation.

--- Free SysLog/Form1.vb
+++ Free SysLog/Form1.vb
@@ -261,7 +261,7 @@ askAgain:
     End Sub
 
     Private Sub UpdateLogCount()
-        NumberOfLogs.Text = $"Number of Log Entries: {logs.Items.Count.ToString("N0")}"
+        NumberOfLogs.Text = $"Number of Log Entries: {logs.Items.Count:N0}"
     End Sub
 
     Private Sub ChkAutoScroll_Click(sender As Object, e As EventArgs) Handles chkAutoScroll.Click
