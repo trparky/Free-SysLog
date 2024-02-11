1cd05a9f0c0fd799491196b5a3bfe2248a9cbba1
Thomas Parkison <trparky@toms-world.org>
2024-02-10 23:34:11 -0500

    Fixed sorting of dates.

--- Free SysLog/listViewSorter.vb
+++ Free SysLog/listViewSorter.vb
@@ -13,8 +13,8 @@ Class ListViewComparer
     ' Compare the items in the appropriate column
     ' for objects x and y.
     Public Function Compare(lvInputFirstListView As Object, lvInputSecondListView As Object) As Integer Implements IComparer.Compare
-        Dim long1, long2 As Long
         Dim strFirstString, strSecondString As String
+        Dim date1, date2 As Date
         Dim lvFirstListView As ListViewItem = lvInputFirstListView
         Dim lvSecondListView As ListViewItem = lvInputSecondListView
 
@@ -24,8 +24,11 @@ Class ListViewComparer
 
         If lvFirstListView.ListView IsNot Nothing Then
             ' Compare them.
-            If Long.TryParse(strFirstString, long1) And Long.TryParse(strSecondString, long2) Then
-                Return If(soSortOrder = SortOrder.Ascending, long1.CompareTo(long2), long2.CompareTo(long1))
+            If intColumnNumber = 0 Then
+                date1 = Date.Parse(strFirstString)
+                date2 = Date.Parse(strSecondString)
+
+                Return If(soSortOrder = SortOrder.Ascending, String.Compare(date1, date2), String.Compare(date2, date1))
             Else
                 Return If(soSortOrder = SortOrder.Ascending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
             End If
