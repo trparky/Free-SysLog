e0e77d73bb9b5a758b47495ba5398d4ae064e34d
Thomas Parkison <trparky@toms-world.org>
2024-03-19 11:52:24 -0400

    Removed MyListViewItem, it's not used in the program anymore.

--- Free SysLog/MyListViewItem.vb
+++ Free SysLog/MyListViewItem.vb
@@ -1,29 +1,4 @@
 ﻿' This class extends the ListViewItem so that I can add more properties to it for my purposes.
-Public Class MyListViewItem
-    Inherits ListViewItem
-    Implements ICloneable
-    Public Property DateObject As Date
-
-    Public Sub New(strInput As String)
-        Me.Text = strInput
-    End Sub
-
-    Public Overrides Function Clone() As Object Implements ICloneable.Clone
-        Dim newListViewItem As New MyListViewItem(Me.Text)
-
-        For index As Short = 1 To Me.SubItems.Count - 1
-            newListViewItem.SubItems.Add(Me.SubItems(index))
-        Next
-
-        With newListViewItem
-            .DateObject = Me.DateObject
-        End With
-
-        Return newListViewItem
-    End Function
-End Class
-
-' This class extends the ListViewItem so that I can add more properties to it for my purposes.
 Public Class MyReplacementsListViewItem
     Inherits ListViewItem
     Implements ICloneable
