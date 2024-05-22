1d5dca9531a4dadb80de97897aba307d6bb2c403
Thomas Parkison <trparky@toms-world.org>
2024-05-21 21:48:50 -0400

    Minor tweaks to the DataGridViewComparer sorting class.

--- Free SysLog/Support Code/DataGridViewComparer.vb
+++ Free SysLog/Support Code/DataGridViewComparer.vb
@@ -10,19 +10,16 @@
     End Sub
 
     Public Function Compare(row1 As MyDataGridViewRow, row2 As MyDataGridViewRow) As Integer Implements IComparer(Of MyDataGridViewRow).Compare
-        Dim strFirstString, strSecondString As String
-        Dim date1, date2 As Date
-
-        ' Get the cell values.
-        strFirstString = If(row1.Cells.Count <= intColumnNumber, "", row1.Cells(intColumnNumber).Value?.ToString())
-        strSecondString = If(row2.Cells.Count <= intColumnNumber, "", row2.Cells(intColumnNumber).Value?.ToString())
-
         ' Compare them.
         If intColumnNumber = 0 Then
-            date1 = row1.DateObject
-            date2 = row2.DateObject
-            Return If(soSortOrder = SortOrder.Descending, Date.Compare(date1, date2), Date.Compare(date2, date1))
+            Dim date1 As Date = row1.DateObject
+            Dim date2 As Date = row2.DateObject
+
+            Return If(soSortOrder = SortOrder.Ascending, Date.Compare(date1, date2), Date.Compare(date2, date1))
         Else
+            Dim strFirstString As String = If(row1.Cells.Count <= intColumnNumber, "", row1.Cells(intColumnNumber).Value?.ToString())
+            Dim strSecondString As String = If(row2.Cells.Count <= intColumnNumber, "", row2.Cells(intColumnNumber).Value?.ToString())
+
             Return If(soSortOrder = SortOrder.Descending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
         End If
 
