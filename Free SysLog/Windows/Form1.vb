af02b76343a839554d19121bbdf025efa887aa4c
Thomas Parkison <trparky@toms-world.org>
2024-09-07 11:32:58 -0400

    Added code to view the raw log data instead of the parsed data that's shown in the datagrid.

--- Free SysLog/Windows/Form1.vb
+++ Free SysLog/Windows/Form1.vb
@@ -458,8 +458,9 @@ Public Class Form1
     Private Sub OpenLogViewerWindow()
         If Logs.Rows.Count > 0 And Logs.SelectedCells.Count > 0 Then
             Dim selectedRow As MyDataGridViewRow = Logs.Rows(Logs.SelectedCells(0).RowIndex)
+            Dim strLogText As String = If(String.IsNullOrWhiteSpace(selectedRow.RawLogData), selectedRow.Cells(ColumnIndex_LogText).Value, selectedRow.RawLogData)
 
-            Using LogViewerInstance As New LogViewer With {.strLogText = selectedRow.Cells(ColumnIndex_LogText).Value, .StartPosition = FormStartPosition.CenterParent, .Icon = Icon}
+            Using LogViewerInstance As New LogViewer With {.strLogText = strLogText, .StartPosition = FormStartPosition.CenterParent, .Icon = Icon}
                 LogViewerInstance.LblLogDate.Text = $"Log Date: {selectedRow.Cells(ColumnIndex_ComputedTime).Value}"
                 LogViewerInstance.LblSource.Text = $"Source IP Address: {selectedRow.Cells(ColumnIndex_IPAddress).Value}"
                 LogViewerInstance.ShowDialog(Me)
