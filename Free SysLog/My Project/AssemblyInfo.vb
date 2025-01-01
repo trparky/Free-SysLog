3441e3014097485e88fd58d8f22ad44be904a409
Thomas Parkison <trparky@toms-world.org>
2024-12-31 22:28:42 -0500

    Updated the version.

--- Free SysLog/My Project/AssemblyInfo.vb
+++ Free SysLog/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("3.7.2.79")>
+<Assembly: AssemblyFileVersion("3.7.3.80")>
