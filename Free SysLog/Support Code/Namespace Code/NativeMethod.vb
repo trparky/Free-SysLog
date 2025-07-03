79fc766725cf2eea0a1a621b9b5ad8058dba5ac2
Thomas Parkison <trparky@toms-world.org>
2025-07-02 21:40:05 -0400

    Added comments to the Native Methods file.

--- Free SysLog/Support Code/Namespace Code/NativeMethod.vb
+++ Free SysLog/Support Code/Namespace Code/NativeMethod.vb
@@ -6,30 +6,37 @@ Namespace NativeMethod
             ' Prevent instantiation
         End Sub
 
+        ' Sets the specified window to the foreground
         <DllImport("User32.dll", EntryPoint:="SetForegroundWindow", SetLastError:=True)>
         Public Shared Function SetForegroundWindow(hWnd As IntPtr) As Boolean
         End Function
 
+        ' Opens an existing process
         <DllImport("kernel32.dll", SetLastError:=True)>
         Friend Shared Function OpenProcess(dwDesiredAccess As ProcessAccessFlags, bInheritHandle As Boolean, dwProcessId As Integer) As IntPtr
         End Function
 
+        ' Closes an open handle
         <DllImport("kernel32.dll", SetLastError:=True)>
         Friend Shared Function CloseHandle(hHandle As IntPtr) As Boolean
         End Function
 
+        ' Retrieves the full path of the executable image for the process
         <DllImport("kernel32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
         Friend Shared Function QueryFullProcessImageName(hprocess As IntPtr, dwFlags As Integer, lpExeName As Text.StringBuilder, ByRef size As Integer) As Boolean
         End Function
 
+        ' Gets a PIDL from a file system path
         <DllImport("shell32.dll", CharSet:=CharSet.Unicode, ExactSpelling:=True, SetLastError:=True)>
         Public Shared Function ILCreateFromPathW(pszPath As String) As IntPtr
         End Function
 
+        ' Opens folder and selects item(s) in Explorer
         <DllImport("shell32.dll", ExactSpelling:=True, SetLastError:=True)>
         Public Shared Function SHOpenFolderAndSelectItems(pidlList As IntPtr, cild As UInteger, children As IntPtr, dwFlags As UInteger) As Integer
         End Function
 
+        ' Frees a PIDL
         <DllImport("shell32.dll", ExactSpelling:=True, SetLastError:=True)>
         Public Shared Sub ILFree(pidlList As IntPtr)
         End Sub
