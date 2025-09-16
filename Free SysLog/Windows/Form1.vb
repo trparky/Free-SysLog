2006370f55dc489873358fd2c32d62c89bd69db2
Thomas Parkison <trparky@toms-world.org>
2025-09-15 20:08:42 -0400

    Fixed a bug on the main window in which the "Save Log" function in the menu wasn't enabled after deleting logs.

--- Free SysLog/Windows/Form1.vb
+++ Free SysLog/Windows/Form1.vb
@@ -776,6 +776,8 @@ Public Class Form1
 
                 Logs.Rows.Add(SyslogParser.MakeLocalDataGridRowEntry($"The user deleted {rowsToDelete.Count:N0} log {If(rowsToDelete.Count = 1, "entry", "entries")}.", Logs))
 
+                BtnSaveLogsToDisk.Enabled = True
+
                 SelectLatestLogEntry()
             End SyncLock
 
