69df86e9f63077e9ed76941f193caa9990be81a8
Thomas Parkison <trparky@toms-world.org>
2025-11-12 07:41:50 -0500

    Deleted the AssemblyInfo.vb file so that it's generated at compile time.

--- Free SysLog/My Project/AssemblyInfo.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
-<Assembly: AssemblyTrademark("")> 
-
-<Assembly: ComVisible(False)>
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("e0cb9c27-da95-4f20-ac04-c6bb7b1af7a3")> 
