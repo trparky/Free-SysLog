8a9ae5106855c3b7bbc37999c7c942c34341aa88
Thomas Parkison <trparky@toms-world.org>
2025-11-29 19:42:06 -0500

    Dropped the use of the Using statements, potentially wierd stuff could happen since I'm returning a disposed row object.

--- Free SysLog/Support Code/My Data Classes.vb
+++ Free SysLog/Support Code/My Data Classes.vb
@@ -12,42 +12,42 @@ Public Class SavedData
     Public BoolAlerted As Boolean = False
 
     Public Function MakeDataGridRow(dataGrid As DataGridView) As MyDataGridViewRow
-        Using MyDataGridViewRow As New MyDataGridViewRow
-            With MyDataGridViewRow
-                .CreateCells(dataGrid)
-                .Cells(ColumnIndex_ComputedTime).Value = Date.Parse(time)
-                .Cells(ColumnIndex_ComputedTime).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
-                .Cells(ColumnIndex_LogType).Value = If(String.IsNullOrWhiteSpace(logType), "", logType)
-                .Cells(ColumnIndex_IPAddress).Value = ip
-                .Cells(ColumnIndex_RemoteProcess).Value = If(String.IsNullOrWhiteSpace(appName), "", appName)
-                .Cells(ColumnIndex_Hostname).Value = If(String.IsNullOrWhiteSpace(hostname), "", hostname)
-                .Cells(ColumnIndex_LogText).Value = log
-                .Cells(ColumnIndex_Alerted).Value = If(BoolAlerted, "Yes", "No")
-                .Cells(ColumnIndex_Alerted).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
-                .Cells(ColumnIndex_Alerted).Style.WrapMode = DataGridViewTriState.True
-                .Cells(ColumnIndex_ServerTime).Value = If(ServerDate = Date.MinValue, "", ToIso8601Format(ServerDate))
-                .DateObject = DateObject
-                .BoolAlerted = BoolAlerted
-                .RawLogData = rawLogData
-                .AlertText = alertText
-                .alertType = alertType
+        Dim MyDataGridViewRow As New MyDataGridViewRow
 
-                If My.Settings.font IsNot Nothing Then
-                    .Cells(ColumnIndex_ComputedTime).Style.Font = My.Settings.font
-                    .Cells(ColumnIndex_LogType).Style.Font = My.Settings.font
-                    .Cells(ColumnIndex_IPAddress).Style.Font = My.Settings.font
-                    .Cells(ColumnIndex_RemoteProcess).Style.Font = My.Settings.font
-                    .Cells(ColumnIndex_Hostname).Style.Font = My.Settings.font
-                    .Cells(ColumnIndex_LogText).Style.Font = My.Settings.font
-                    .Cells(ColumnIndex_Alerted).Style.Font = My.Settings.font
-                    .Cells(ColumnIndex_ServerTime).Style.Font = My.Settings.font
-                End If
+        With MyDataGridViewRow
+            .CreateCells(dataGrid)
+            .Cells(ColumnIndex_ComputedTime).Value = Date.Parse(time)
+            .Cells(ColumnIndex_ComputedTime).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .Cells(ColumnIndex_LogType).Value = If(String.IsNullOrWhiteSpace(logType), "", logType)
+            .Cells(ColumnIndex_IPAddress).Value = ip
+            .Cells(ColumnIndex_RemoteProcess).Value = If(String.IsNullOrWhiteSpace(appName), "", appName)
+            .Cells(ColumnIndex_Hostname).Value = If(String.IsNullOrWhiteSpace(hostname), "", hostname)
+            .Cells(ColumnIndex_LogText).Value = log
+            .Cells(ColumnIndex_Alerted).Value = If(BoolAlerted, "Yes", "No")
+            .Cells(ColumnIndex_Alerted).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .Cells(ColumnIndex_Alerted).Style.WrapMode = DataGridViewTriState.True
+            .Cells(ColumnIndex_ServerTime).Value = If(ServerDate = Date.MinValue, "", ToIso8601Format(ServerDate))
+            .DateObject = DateObject
+            .BoolAlerted = BoolAlerted
+            .RawLogData = rawLogData
+            .AlertText = alertText
+            .alertType = alertType
 
-                .DefaultCellStyle.Padding = New Padding(0, 2, 0, 2)
-            End With
+            If My.Settings.font IsNot Nothing Then
+                .Cells(ColumnIndex_ComputedTime).Style.Font = My.Settings.font
+                .Cells(ColumnIndex_LogType).Style.Font = My.Settings.font
+                .Cells(ColumnIndex_IPAddress).Style.Font = My.Settings.font
+                .Cells(ColumnIndex_RemoteProcess).Style.Font = My.Settings.font
+                .Cells(ColumnIndex_Hostname).Style.Font = My.Settings.font
+                .Cells(ColumnIndex_LogText).Style.Font = My.Settings.font
+                .Cells(ColumnIndex_Alerted).Style.Font = My.Settings.font
+                .Cells(ColumnIndex_ServerTime).Style.Font = My.Settings.font
+            End If
 
-            Return MyDataGridViewRow
-        End Using
+            .DefaultCellStyle.Padding = New Padding(0, 2, 0, 2)
+        End With
+
+        Return MyDataGridViewRow
     End Function
 End Class
 
@@ -77,41 +77,41 @@ Public Class AlertsHistory
     Public alertType As AlertType
 
     Public Function MakeDataGridRow(dataGrid As DataGridView) As AlertsHistoryDataGridViewRow
-        Using AlertsHistoryDataGridViewRow As New AlertsHistoryDataGridViewRow
-            With AlertsHistoryDataGridViewRow
-                .CreateCells(dataGrid)
-                .Cells(0).Value = strTime
-                .Cells(0).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+        Dim AlertsHistoryDataGridViewRow As New AlertsHistoryDataGridViewRow
 
-                If alertType = AlertType.ErrorMsg Then
-                    .Cells(1).Value = "Error"
-                ElseIf alertType = AlertType.Warning Then
-                    .Cells(1).Value = "Warning"
-                ElseIf alertType = AlertType.Info Then
-                    .Cells(1).Value = "Information"
-                Else
-                    .Cells(1).Value = ""
-                End If
+        With AlertsHistoryDataGridViewRow
+            .CreateCells(dataGrid)
+            .Cells(0).Value = strTime
+            .Cells(0).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
 
-                .Cells(1).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
-                .Cells(2).Value = strAlertText
+            If alertType = AlertType.ErrorMsg Then
+                .Cells(1).Value = "Error"
+            ElseIf alertType = AlertType.Warning Then
+                .Cells(1).Value = "Warning"
+            ElseIf alertType = AlertType.Info Then
+                .Cells(1).Value = "Information"
+            Else
+                .Cells(1).Value = ""
+            End If
 
-                If My.Settings.font IsNot Nothing Then
-                    .Cells(0).Style.Font = My.Settings.font
-                    .Cells(1).Style.Font = My.Settings.font
-                    .Cells(2).Style.Font = My.Settings.font
-                End If
+            .Cells(1).Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .Cells(2).Value = strAlertText
 
-                .strRawLog = strRawLog
-                .strIP = strIP
-                .strLog = strLog
-                .strTime = strTime
-                .strAlertText = strAlertText
-                .alertType = alertType
-            End With
+            If My.Settings.font IsNot Nothing Then
+                .Cells(0).Style.Font = My.Settings.font
+                .Cells(1).Style.Font = My.Settings.font
+                .Cells(2).Style.Font = My.Settings.font
+            End If
 
-            Return AlertsHistoryDataGridViewRow
-        End Using
+            .strRawLog = strRawLog
+            .strIP = strIP
+            .strLog = strLog
+            .strTime = strTime
+            .strAlertText = strAlertText
+            .alertType = alertType
+        End With
+
+        Return AlertsHistoryDataGridViewRow
     End Function
 End Class
 
