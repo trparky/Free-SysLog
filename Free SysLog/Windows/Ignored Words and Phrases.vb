e8cfa939e91dd144d54b701bfc69be936443c7d5
Thomas Parkison <trparky@toms-world.org>
2025-12-07 12:27:25 -0500

    Removed a line of debug code.

--- Free SysLog/Windows/Ignored Words and Phrases.vb
+++ Free SysLog/Windows/Ignored Words and Phrases.vb
@@ -191,8 +191,6 @@ Public Class IgnoredWordsAndPhrases
     Private Sub AutoRefreshTimer_Tick(sender As Object, e As EventArgs)
         If boolCurrentlyEditing OrElse (Not NativeMethod.NativeMethods.GetForegroundWindow() = Me.Handle And ChkRefreshOnlyIfActive.Checked) Then Exit Sub
 
-        Debug.WriteLine("Ignored Words and Phrases Auto-Refresh Triggered")
-
         ' Prevent re-entry if refresh takes longer than the interval
         AutoRefreshTimer.Stop()
 
